18f12af408315e2ed1feebf121c1b5809f4a301a
jaycer95 <jacer.dabbabi95@gmail.com>
2020-09-07 06:54:53 -0700

    adv

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#Custom header with puppet
+exec {'http header':
+command  => 'sudo apt-get update -y;
+sudo apt-get install nginx -y;
+sudo echo "server {                                                                  
+    listen 80;                                                                  
+    listen [::]:80 default_server;                                              
+    root   /var/www//html;                                                      
+    index index.nginx-debian.html index.nginx-debian.html;
+    add_header X-Served-By $HOSTNAME;                    
+                                                                     
+}" > /etc/nginx/sites-available/default;
+service nginx restart',
+provider => shell,
+}
