76e9491e7039311f5a2f26e14ae74291fc0c12d4
jaycer95 <jacer.dabbabi95@gmail.com>
2020-09-16 02:57:34 -0700

    finish

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Python script that, using a given REST API, for a given employee ID,
+returns information about his/her TODO list progress """
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    r1 = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    r2 = requests.get('https://jsonplaceholder.typicode.com/users')
+    todo = r1.json()
+    user = r2.json()
+    dic = {}
+    with open('todo_all_employees' + '.json', mode='w') as f:
+        for users in user:
+            USERNAME = users.get("username")
+            Id = users.get("id")
+            dic[Id] = []
+            for tasks in todo:
+                if Id == tasks["userId"]:
+                    inside = {}
+                    inside = {"username": USERNAME, "task": tasks.get("title"),
+                            "completed": tasks.get("completed")}
+                    dic[Id].append(inside)
+        json.dump(dic, f)
