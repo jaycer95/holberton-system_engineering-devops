9e3f4588badf5834f1e2f6ea83b4b6f2cd497b48
jaycer95 <jacer.dabbabi95@gmail.com>
2020-10-08 13:24:02 -0700

    0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+#the_sky_is_the_limit_not
+
+exec { 'Ulimit nginx':
+  command => 'sudo echo "ULIMIT=\"-n 4096\"" >> /etc/default/nginx,
+  path    => '/bin/',
+}
+
+-> exec { 'restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/',
+}
