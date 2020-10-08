66948ebb5f8972287d8662343dcacb1dfa5565e4
jaycer95 <jacer.dabbabi95@gmail.com>
2020-10-08 14:04:52 -0700

    lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,7 @@
 #the_sky_is_the_limit_not
 
 exec { 'Ulimit nginx':
-  command => 'sudo echo "ULIMIT=\"-n 4096\"" >> /etc/default/nginx ;
-sudo service nginx restart',
-  provider=> shell,
+  command  => 'sudo echo "ULIMIT=\"-n 4096\"" >> /etc/default/nginx ; sudo service nginx restart',
+  provider => shell,
 }
 
